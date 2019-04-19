269751497a87b96c0a87820d26a8d9cc26af8c42
Oz Parl <ozzy7o@icloud.com>
2019-04-18 20:50:44 -0400

    Add files via upload

---
+++ simple guessing game/script.js
@@ -0,0 +1,13 @@
+var num = prompt("Guess a number!")
+
+if (num === "7"){
+    alert("Your guess was correct")
+
+}
+else if (num < 7){
+    alert("Your guess is too low")
+}
+else if (num > 7){
+    alert("Your guess is too High");
+}
+
